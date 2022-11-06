24c4ed957877c208bec18a8cc00d8cb157d86cc1
IrfanNasyakban <irfanna.syakban098@gmail.com>
2022-11-06 12:17:10 +0700

    update

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	authcontroller "github.com/IrfanNasyakban/Project-Laporan/go-auth/controller"
+	authcontroller "github.com/IrfanNasyakban/Project-Laporan/controller"
 )
 
 func main() {
